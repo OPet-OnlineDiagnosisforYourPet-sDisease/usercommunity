58288bcbc19f6f49bcf9478c49b941220406ec46
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-06-02 22:09:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -91,17 +91,21 @@ app.post('/login', (req, res) => {
 
         // Jika email dan password cocok
         if (results.length > 0) {
+            // Mengambil email dan username pengguna
+            const { email, name } = results[0];
+
             // Generate token JWT
             const user = { email };
             const token = generateToken(user);
             
-            return res.status(200).json({ message: 'Login successful', token });
+            return res.status(200).json({ message: 'Login successful', email, username: name, token });
         }
 
         // Jika email dan password tidak cocok
         return res.status(200).json({ message: 'Invalid email or password' });
     });
 });
+
 
 // Endpoint untuk menambahkan story baru
 app.post('/stories', verifyToken, (req, res) => {
